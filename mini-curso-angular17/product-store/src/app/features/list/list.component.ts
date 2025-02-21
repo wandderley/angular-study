026c428ae1de666006529ce8c1c287673229acba
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-21 11:32:50 -0300

    feat: adiciona Card para mostrar produto

--- mini-curso-angular17/product-store/src/app/features/list/list.component.ts
+++ mini-curso-angular17/product-store/src/app/features/list/list.component.ts
@@ -2,10 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -14,8 +19,8 @@ export class ListComponent {
 
   products: Product[] = []
 
-  ngOnInit(){
-    this.productsService.getAll().subscribe((products)=>{
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
   }
