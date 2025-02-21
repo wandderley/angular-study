43a3a4968fdd371d7472c129bc061c2a5965aff0
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-21 14:07:38 -0300

    feat: cria componente para adicionar produto

---
+++ mini-curso-angular17/product-store/src/app/features/create/create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  imports: [],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss'
+})
+export class CreateComponent {
+
+}
