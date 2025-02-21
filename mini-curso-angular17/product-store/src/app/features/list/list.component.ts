2bf5c0048572508f3e85b9d215b480caf4b24e41
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-21 14:03:49 -0300

    feat: adiciona link para criar um produto

--- mini-curso-angular17/product-store/src/app/features/list/list.component.ts
+++ mini-curso-angular17/product-store/src/app/features/list/list.component.ts
@@ -2,13 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
-  imports: [CardComponent],
+  imports: [
+    CardComponent,
+    RouterLink,
+    MatButtonModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
