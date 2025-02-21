832828beb549d774277a9df9fdad901eb3ebe9a3
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-21 08:57:33 -0300

    chore: adiciona Material

--- mini-curso-angular17/product-store/src/app/shared/components/header/header.component.ts
+++ mini-curso-angular17/product-store/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from `@angular/material/toolbar`;
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
