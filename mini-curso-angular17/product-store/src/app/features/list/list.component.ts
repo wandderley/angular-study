d503d7e950832e908a424901d64501162e9063ce
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-21 12:26:35 -0300

    refactor: refatora listagem de produto criando um componente para renderizar o mesmo

--- mini-curso-angular17/product-store/src/app/features/list/list.component.ts
+++ mini-curso-angular17/product-store/src/app/features/list/list.component.ts
@@ -4,13 +4,11 @@ import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
-  imports: [
-    MatCardModule,
-    MatButtonModule
-  ],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
