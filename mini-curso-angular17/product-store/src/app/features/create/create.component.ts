62715c9fcd33e3894593b4f4649c0d7794757c4b
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-24 09:15:27 -0300

    feat: adiciona mensagem de feedback ao criar produto

--- mini-curso-angular17/product-store/src/app/features/create/create.component.ts
+++ mini-curso-angular17/product-store/src/app/features/create/create.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -18,8 +20,10 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
 
-  productsService = inject(ProductsService)
-  
+  productsService = inject(ProductsService);
+  matSnackbar = inject(MatSnackBar);
+  router = inject(Router);
+
   form = new FormGroup({
     title: new FormControl<string>('', {
       nonNullable: true,
@@ -27,12 +31,18 @@ export class CreateComponent {
     }),
   })
 
-  onSubmit(){
+  onSubmit(){  
     this.productsService.post({
       title: this.form.controls.title.value,
     })
     .subscribe(() => {
-      alert("Success!");
+      this.matSnackbar.open('Produto criado com sucesso!', 'OK', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top'
+      });
+
+      this.router.navigateByUrl('/');
     })
   }
 }
