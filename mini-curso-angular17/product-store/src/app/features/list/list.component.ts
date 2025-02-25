9d9d84e1795d049b8aca3f708e51b35f0328ea30
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-24 22:49:00 -0300

    feat: adiciona funcionalidade de deletar produto

--- mini-curso-angular17/product-store/src/app/features/list/list.component.ts
+++ mini-curso-angular17/product-store/src/app/features/list/list.component.ts
@@ -1,18 +1,47 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import {MatCardModule} from '@angular/material/card';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+    <h2 mat-dialog-title>Deletar Produto</h2>
+    <mat-dialog-content>
+      Tem certeza que deseja deletar este produto?
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button (click)="onNo()">Não</button>
+      <button mat-button (click)="onYes()" cdkFocusInitial>Sim</button>
+  </mat-dialog-actions>
+  `,
+  imports: [MatButtonModule, MatDialogModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmationDialogComponent {
+
+  matDialogRef = inject(MatDialogRef);
+
+  onNo() {
+    this.matDialogRef.close(false)
+  }
+  onYes() {
+    this.matDialogRef.close(true)
+  }
+}
+
 
 @Component({
   selector: 'app-list',
   imports: [
     CardComponent,
     RouterLink,
-    MatButtonModule
+    MatButtonModule,
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
@@ -21,6 +50,7 @@ export class ListComponent {
 
   productsService = inject(ProductsService);
   router = inject(Router);
+  matDialog = inject(MatDialog);
 
   products: Product[] = [];
 
@@ -32,5 +62,19 @@ export class ListComponent {
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
+  }
+
+  onDelete(product: Product) {
+    this.matDialog
+      .open(ConfirmationDialogComponent)
+      .afterClosed()
+      .pipe(filter((answer) => answer === true))
+      .subscribe(() => {
+        this.productsService.delete(product.id).subscribe(()=>{
+          this.productsService.getAll().subscribe((products) => {
+            this.products = products;
+          });
+        })
+      });
   }
 }
--- mini-curso-angular17/product-store/src/app/shared/services/products.service.ts
+++ mini-curso-angular17/product-store/src/app/shared/services/products.service.ts
@@ -25,4 +25,9 @@ export class ProductsService {
   put(id: string, payload: ProductPayload){
     return this.httpClient.put(`/api/products/${id}`, payload)
   }
+
+  delete(id: string){
+      return this.httpClient.delete(`/api/products/${id}`)
+
+  }
 }
